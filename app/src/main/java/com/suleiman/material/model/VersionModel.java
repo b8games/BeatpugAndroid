b091a98bf576eb8e0321308cd94d3614c5656546
Umut Kurt <umutkurtr@outlook.com>
2016-11-28 19:48:08 +0300

    Selam ilker

--- app/src/main/java/com/suleiman/material/model/VersionModel.java
+++ app/src/main/java/com/suleiman/material/model/VersionModel.java
@@ -5,7 +5,6 @@ package com.suleiman.material.model;
  */
 public class VersionModel {
     public String name;
-
     public static final String[] data = {"Cupcake", "Donut", "Eclair",
             "Froyo", "Gingerbread", "Honeycomb",
             "Icecream Sandwich", "Jelly Bean", "Kitkat", "Lollipop"};
