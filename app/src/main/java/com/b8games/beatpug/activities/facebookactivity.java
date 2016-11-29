d2b4c6214cd1de11ab313f392df585022ad124c4
Umut Kurt <umutkurtr@outlook.com>
2016-11-29 21:05:17 +0300

    ilker yine yakıyorsun

--- app/src/main/java/com/b8games/beatpug/activities/facebookactivity.java
+++ app/src/main/java/com/b8games/beatpug/activities/facebookactivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.b8games.beatpug.R;
 
@@ -19,7 +20,11 @@ public class facebookactivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.facebookactivity, container, false);
 
+        WebView wv = (WebView) view.findViewById(R.id.facebook_webview);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("file:///android_asset/facebook.html");
         return view;
+
     }
 
 }
